beae05024705b8f2c844b28f823dcb13db17f205
shinyeeun789 <tlsdpdms789@naver.com>
2023-10-30 12:45:58 +0900

    프로젝트 생성(gradle, maven)

---
+++ test01/src/main/java/com/chunjae/test01/mapper/Test1Mapper.java
@@ -0,0 +1,16 @@
+package com.chunjae.test01.mapper;
+
+import com.chunjae.test01.domain.Test1;
+import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Select;
+
+import java.util.List;
+
+@Mapper
+public interface Test1Mapper {
+    @Select("select * from test1")
+    public List<Test1> getList();
+
+    public List<Test1> getList2();
+
+}
