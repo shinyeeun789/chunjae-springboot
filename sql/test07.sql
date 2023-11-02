399236a073f26d177735f1b4332365b9386432bd
shinyeeun789 <tlsdpdms789@naver.com>
2023-11-02 17:45:38 +0900

    sql문 수정

--- sql/test07.sql
+++ sql/test07.sql
@@ -2,11 +2,23 @@ CREATE DATABASE tsherpa;
 
 USE tsherpa;
 
-CREATE TABLE ROLE(
+CREATE TABLE role(
 	role_id INT PRIMARY KEY AUTO_INCREMENT,
 	role VARCHAR(255) DEFAULT NULL
 );
 
 CREATE TABLE user(
-	
+	user_id INT PRIMARY KEY AUTO_INCREMENT,
+	active INT DEFAULT 0,
+	login_id VARCHAR(255) NOT NULL,
+	user_name VARCHAR(255) NOT NULL,
+	password VARCHAR(300) NOT NULL,
+	password_confirm VARCHAR(200) NOT NULL
 );
+
+CREATE TABLE user_role(
+	user_id INT NOT NULL,
+	role_id INT NOT NULL,
+	PRIMARY KEY (user_id, role_id)
+);
+
