cff983884b661017e6ffd6be28fed41bd724822a
shinyeeun789 <tlsdpdms789@naver.com>
2023-11-02 15:13:02 +0900

    Restful API 생성

---
+++ test04/src/main/java/com/chunjae/test04/exception/DataIntefrityViolationException.java
@@ -0,0 +1,9 @@
+package com.chunjae.test04.exception;
+
+public class DataIntefrityViolationException extends RuntimeException {
+
+    public DataIntefrityViolationException(String message) {
+        super(message);
+    }
+
+}
